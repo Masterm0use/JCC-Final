5e9d623712996949c08116195a3c5e375946c66e
Beerkens,Ruud R.J.M. <r.beerkens@student.fontys.nl>
2016-03-29 06:49:49 +0200

    gekke sets constructie

--- VerzamelAppFX/src/verzamelappfx/Set.java
+++ VerzamelAppFX/src/verzamelappfx/Set.java
@@ -11,7 +11,7 @@ import java.util.*;
  */
 public class Set {
     private String naam;
-    
+    private ArrayList<Set> subSets;
     public String getNaam(){return naam;}
     
     public void setNaam(String value)
@@ -37,9 +37,26 @@ public class Set {
         this.naam = naam;
         this.jaar = jaar;
         this.voorwerpen = new ArrayList<>();
+        this.subSets = new ArrayList<>();
     }
     
     public void add(Voorwerp item){
         voorwerpen.add(item);
     }
+    public Voorwerp FindVoorwerp(String naam){
+        for (Voorwerp voor : voorwerpen) {
+            if(voor.getNaam()==naam){
+            return voor;
+            }
+            else{
+                for (Set subSet : subSets) {
+                    if(subSet.FindVoorwerp(naam) != null){
+                        return subSet.FindVoorwerp(naam);
+                    }
+                }
+            }
+        }
+    return null;
+    }
+    
 }
